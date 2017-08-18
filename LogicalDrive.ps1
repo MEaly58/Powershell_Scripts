8ede9961576249a1808a81fde182d7fa04db4162
Mathew Ealy <mealy58@gmail.com>
2017-08-18 11:27:21 -0400

    Create LogicalDrive.ps1

---
+++ LogicalDrive.ps1
@@ -0,0 +1,9 @@
+Write-Host "Drive information for $env:ComputerName"
+Get-WmiObject -Class Win32_LogicalDisk |
+    Where-Object {$_.DriveType -ne 5} |
+    Sort-Object -Property Name | 
+    Select-Object Name, VolumeName, FileSystem, Description, VolumeDirty, `
+        @{"Label"="DiskSize(GB)";"Expression"={"{0:N}" -f ($_.Size/1GB) -as [float]}}, `
+        @{"Label"="FreeSpace(GB)";"Expression"={"{0:N}" -f ($_.FreeSpace/1GB) -as [float]}}, `
+        @{"Label"="%Free";"Expression"={"{0:N}" -f ($_.FreeSpace/$_.Size*100) -as [float]}} |
+    Format-Table -AutoSize
