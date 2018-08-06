f487f03794b16644d0c92bc92d53f0849b839850
Mathew Ealy <Mealy.IT@Gmail.com>
2018-08-06 11:12:07 -0400

    Update CatFacts.ps1
    Added best practice header

--- CatFacts.ps1
+++ CatFacts.ps1
@@ -1,3 +1,15 @@
+<#!
+.SYNOPSIS
+	Send people cat facts
+.DESCRIPTION
+	This is a gag script to open cat facts and turn the speakers on. I recommend using a shortened url and renaming the script to something less suspicies 
+.NOTES
+	File Name: CatFacts.ps1
+	Author: Mathew Ealy
+	Requires Powershell 2.0
+.LINK
+	https://github.com/MEaly58
+#>
 #Run this every 1/2 hour and in an 8 hour work day there will be approximately 3 times per day that your victim hears a cat fact
 if ((Get-Random -Maximum 10000) -lt 1875) {
     Add-Type -AssemblyName System.Speech
