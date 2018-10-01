e15b0d9e1a31b2c993bd0c96a54e264456995598
Mathew Ealy <Mealy.IT@Gmail.com>
2018-10-01 13:22:03 -0400

    Delete ConvertVHD2VHDX.ps1

--- ConvertVHD2VHDX.ps1
+++
@@ -1,4 +0,0 @@
- #Powershell to convert .vhd to .vhdx. Time to perform is dependent on the size of the vhd file.
-Convert-VHD –Path 'C:\ClusterStorage\Volume1\Server\Server.vhd' –DestinationPath 'C:\ClusterStorage\Volume1\Server\Server.vhdx' 
-# Powershell to convert from 512 sector size to 4096.  Goes very fast.
-set-vhd 'C:\ClusterStorage\Volume1\Server\Server.vhdx' -PhysicalSectorSizeBytes 4096
