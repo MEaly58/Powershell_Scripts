0c5d6b55cf2446224e65e57a7bbe7e9f98f1383a
Mathew Ealy <mealy58@gmail.com>
2020-10-14 13:49:27 -0400

    Update CatFacts.ps1

--- CatFacts.ps1
+++ CatFacts.ps1
@@ -14,7 +14,7 @@
 if ((Get-Random -Maximum 10000) -lt 1875) {
     Add-Type -AssemblyName System.Speech
     $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-    $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'http://catfacts-api.appspot.com/api/facts')).facts
+    $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'https://cat-fact.herokuapp.com/api/facts')).facts
     $SpeechSynth.Speak("did you know?")
     $SpeechSynth.Speak($CatFact)
 }  
